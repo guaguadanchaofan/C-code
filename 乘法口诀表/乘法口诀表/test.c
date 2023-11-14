de77d9a352bcfc00449afe1401d359edb3a2c603
guaguadanchaofan <2323398255@qq.com>
2023-11-14 13:36:16 +0800

    更新

---
+++ 乘法口诀表/乘法口诀表/test.c
@@ -0,0 +1,15 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (i = 1; i < 10; i++)
+	{
+		for (j = 1; j <=i; j++)
+		{
+			printf("%d*%d=%2d ", i, j,i*j);
+		}
+		printf("\n");
+	}
+}
