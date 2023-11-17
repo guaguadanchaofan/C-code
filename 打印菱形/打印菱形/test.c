51d4a85b6446c8b74c63b6e94c9d3b156481356b
guaguadanchaofan <2323398255@qq.com>
2023-11-17 13:50:44 +0800

    打印n行的菱形

---
+++ 打印菱形/打印菱形/test.c
@@ -0,0 +1,38 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int i = 0;
+	int j = 0;
+	for (i = n-1; i > 1; i--)//…œ∞Î∂Œ
+	{
+		int t = i;
+		while (t - 1)
+		{
+			printf(" ");
+			t--;
+		}
+		for (j = i; j <= n + 1 - i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (i = 1; i <= n; i++)//œ¬∞Î∂Œ
+	{
+		int t = i;
+		while (t-1)
+		{
+			printf(" ");
+			t--;
+		}
+		for (j = i; j <= n+1 - i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
