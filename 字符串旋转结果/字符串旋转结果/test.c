c263345e09dd4ac62fd7ba21d7bf70562635738c
guaguadanchaofan <2323398255@qq.com>
2023-11-20 22:18:33 +0800

    bug修复

--- 字符串旋转结果/字符串旋转结果/test.c
+++ 字符串旋转结果/字符串旋转结果/test.c
@@ -1,18 +1,18 @@
-#define  _CRT_SECURE_NO_WARNINGS
+#define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
 
 int find(const char* S1, char* S2)
 {
-	char tmp[20] = { 0 };
+	char tmp[200] = { 0 };
 	strcpy(tmp, S1);
-	strcat(tmp, S2);
-	return strstr(tmp, find) != NULL;
+	strcat(tmp, S1);
+	return strstr(tmp, S2) != NULL;
 }
 int main()
 {
-	char S1[5] = "ABCDE";
-	char S2[5] = "abcde";
+	char S1[] = "ABCDE";
+	char S2[] = "bcdef";
 	int ret= find(S1, S2);
 	if (ret == 1)
 		printf("yes");
