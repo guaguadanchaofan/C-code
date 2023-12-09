bbfa96b6e706f54b59b5f34e3c2205bfbb02d94b
guaguadanchaofan <2323398255@qq.com>
2023-12-09 21:42:34 +0800

    相同的树

---
+++ Same Tree/Same Tree/test.c
@@ -0,0 +1,28 @@
+#define  _CRT_SECURE_NO_WARNINGS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL)
+    {
+        return true;
+    }
+    if (p != NULL && q == NULL)
+    {
+        return false;
+    }
+    if (p == NULL && q != NULL)
+    {
+        return false;
+    }
+    if (p->val != q->val)
+    {
+        return false;
+    }
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
