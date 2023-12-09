c2b2bbc93840cae663fdc2075a209235f9bbb184
guaguadanchaofan <2323398255@qq.com>
2023-12-09 21:08:07 +0800

    翻转二叉树

---
+++ Invert Binary Tree/Invert Binary Tree/test.c
@@ -0,0 +1,21 @@
+#define  _CRT_SECURE_NO_WARNINGS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    struct TreeNode* tmp = root->left;
+    root->left = root->right;
+    root->right = tmp;
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
