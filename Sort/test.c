43d72c042345655b10a0f471ee2ea2fe9aa8425f
guaguadanchaofan <2323398255@qq.com>
2023-12-15 16:11:49 +0800

    排序

---
+++ Sort/test.c
@@ -0,0 +1,81 @@
+#include "Sort.h"
+
+
+void TestOP()
+{
+	srand(time(0));
+	const int N = 10000;
+	int* a1 = (int*)malloc(sizeof(int) * N);
+	int* a2 = (int*)malloc(sizeof(int) * N);
+	int* a3 = (int*)malloc(sizeof(int) * N);
+	int* a4 = (int*)malloc(sizeof(int) * N);
+	int* a5 = (int*)malloc(sizeof(int) * N);
+	int* a6 = (int*)malloc(sizeof(int) * N);
+	int* a7 = (int*)malloc(sizeof(int) * N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		a1[i] = rand();
+		a2[i] = a1[i];
+		a3[i] = a1[i];
+		a4[i] = a1[i];
+		a5[i] = a1[i];
+		a6[i] = a1[i];
+		a7[i] = a1[i];
+	}
+
+	int begin1 = clock();
+	//InsertSort(a1, N);
+	int end1 = clock();
+
+	int begin2 = clock();
+	//ShellSort(a2, N);
+	int end2 = clock();
+
+	int begin3 = clock();
+	//SelectSort(a3, N);
+	int end3 = clock();
+
+	int begin4 = clock();
+	//HeapSort(a4, N);
+	int end4 = clock();
+
+	int begin5 = clock();
+	//QuickSort(a4, 0, N - 1);
+	int end5 = clock();
+
+	int begin6 = clock();
+	MergeSort(a6, N);
+	int end6 = clock();
+
+	int begin7 = clock();
+	//BubbleSort(a7, N);
+	int end7 = clock();
+
+	printf("InsertSort:%d\n", end1 - begin1);
+	printf("ShellSort:%d\n", end2 - begin2);
+	printf("SelectSort:%d\n", end3 - begin3);
+	printf("HeapSort:%d\n", end4 - begin4);
+	printf("BubbleSort:%d\n", end7 - begin7);
+	printf("QuickSort:%d\n", end5 - begin5);
+	printf("MergeSort:%d\n", end6 - begin6);
+
+	free(a1);
+	free(a2);
+	free(a3);
+	free(a4);
+	free(a5);
+	free(a6);
+	free(a7);
+}
+
+int main()
+{
+	int a[] = { 3213,454,6,6765,879,321,534,657,876,45431,431213,215,5,3 };
+	//TestMergeSort();
+	//TestQuickSort();
+	//TestOP();
+	//QuickSort(a, 0, sizeof(a) / sizeof(int) - 1);
+	MergeSort(a, sizeof(a) / sizeof(int));
+	return 0;
+}
